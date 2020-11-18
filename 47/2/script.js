176e4141d306f6fdeea0801a690a34aec13f63de
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-11-18 13:41:07 +0100

    Uppgift 2

---
+++ 47/2/script.js
@@ -0,0 +1,7 @@
+sumThree = (a, b, c) => a + b + c
+console.log(sumThree(5, 6, 7)) // 18
+
+isZero = a => a === 0
+console.log(isZero(5)) // false
+console.log(isZero(0)) // true
+console.log(isZero("0")) // false
