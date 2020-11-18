c7a40889ee4515c032df8f7e3d9babfb52b4f3c2
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-11-18 13:33:04 +0100

    Uppgift 1

---
+++ 46/3/script.js
@@ -0,0 +1,9 @@
+const animal = {species: 'Horse', name: 'Sixten', age: 5}
+
+console.log(animal)
+
+animal.age = animal.age + 1
+animal.age = 6
+
+console.log(animal)
+
--- 47/1/script.js
+++ 47/1/script.js
@@ -1,8 +1,16 @@
 
 // låt i vara 0
-
-// while i < 6
 //      logga ut i
 //      öka i med 1
-
 // logga ut "Färdig!"
+
+let i = 0
+
+while (i < 6) {
+    console.log(i++)
+}
+console.log('Färdig!')
+
+
+
+
