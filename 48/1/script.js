f402391e4a176c89354c6f4368502d21970af53b
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-11-25 12:04:18 +0100

    Uppgift 1

--- 48/1/script.js
+++ 48/1/script.js
@@ -1,3 +1,10 @@
 // Deklarera en variabel name och ge den värdet av ditt namn
 
 // Skriv en for-loop som loggar ut "Hejsan <ditt namn här>!!" 500 gånger
+
+const name = 'Jonathan'
+
+for (let n = 0; n < 500; n++) {
+    console.log(`Hejsan ${name}!!`)
+    
+}
